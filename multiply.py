4924b1b5eaacc1d2001602415e5477588947960f
KaiGeller <gellerka@oregonstate.edu>
2022-04-27 18:24:41 -0700

    1

---
+++ multiply.py
@@ -0,0 +1,9 @@
+#Author Kai Geller
+#GitHub Username: KaiGeller
+#4/27/2022
+# Assignment number 5 to multiply two terms together
+def multiply(num1, num2):
+    """This function multiplies the two inputs together"""
+    if num2 == 0:
+        return 0
+    return multiply(num1, num2)
